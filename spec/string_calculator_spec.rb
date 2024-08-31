7b89867f40e2844da9d121d433caf7113cdbce49
diksha_dhakate <diksha_dhakate@persistent.com>
2024-08-31 14:05:36 +0530

    Testing the add method for single input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../string_calculator'
 
 describe '#add' do
-	it 'returns 0 for an empty string' do
-		expect(add("")).to eq(0)
+	it 'returns the number for a single input' do
+		expect(add("5")).to eq(5)
 	end
 end
