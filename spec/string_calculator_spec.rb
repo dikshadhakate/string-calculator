ac0be4c5a1ee44d4011d87bac48652c0fe7ed9ed
diksha_dhakate <diksha_dhakate@persistent.com>
2024-08-31 14:31:27 +0530

    description added in rspec for support different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../string_calculator'
 
 describe '#add' do
-	it 'returns the sum of multiple numbers' do
+	it 'supports custom delimiter' do
 		expect(add("//;\n1;2")).to eq(3)
 	end
 end
